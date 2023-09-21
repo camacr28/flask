116c94a2bee7d7f0d54811b1728bbf5a2a8243ae
camacr28 <camacr28@gmail.com>
2023-09-21 22:49:57 +0200

    feat: Preparamos las rutas de la app

---
+++ balance/views.py
@@ -0,0 +1,34 @@
+from . import app
+
+
+@app.route('/')
+def home():
+    """
+    Mostrar lista de movimientos cargados.
+    """
+
+    return 'Lista de movimientos.'
+
+
+@app.route('/nuevo')
+def add_movement():
+    """
+    Crear movimiento nuevo y guardarlo en archivo .csv.
+    """
+    return 'Agregar nuevo movimiento.'
+
+
+@app.route('/modificar')
+def update():
+    """
+    Editar datos de un movimiento creado previamente.
+    """
+    return 'Actualizar movimiento.'
+
+
+@app.route('/borrar')
+def delete():
+    """
+    Borra un movimiento existente.
+    """
+    return 'Borrar movimiento.'
