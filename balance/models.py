b21c50d569b47cf128192a7dad0ed360f36ec4b9
camacr28 <camacr28@gmail.com>
2023-09-26 20:48:02 +0200

    Añadido color a errores

--- balance/models.py
+++ balance/models.py
@@ -1,21 +1,32 @@
 import csv
 from datetime import date
+
 from . import RUTA_FICHERO
 
 
 class Movimiento:
     def __init__(self, fecha, concepto, tipo, cantidad):
+
+        self.errores = []
+
         try:
             self.fecha = date.fromisoformat(fecha)
         except ValueError:
             self.fecha = None
-            print(f'La fecha {fecha} no es válida.')
+            self.errores.append(f'La fecha {fecha} no es una fecha válida')
         self.concepto = concepto
         self.tipo = tipo
         self.cantidad = cantidad
 
+    @property
+    def has_errors(self):
+        return len(self.errores) > 0
+
     def __str__(self):
-        return f'{self.fecha} | {self.concepto} | {self.tipo} | {self.cantidad}'
+        return f'{self.fecha}\t{self.concepto}\t{self.tipo}\t{self.cantidad}'
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class ListaMovimientos:
@@ -31,11 +42,15 @@ class ListaMovimientos:
                     fila['fecha'],
                     fila['concepto'],
                     fila['ingreso_gasto'],
-                    fila['cantidad'])
-                self.lista_movimientos.append(fila)
+                    fila['cantidad']
+                )
+                self.lista_movimientos.append(movimiento)
 
     def __str__(self):
         result = ''
         for mov in self.lista_movimientos:
             result += f'\n{mov}'
         return result
+
+    def __repr__(self):
+        return self.__str__()
